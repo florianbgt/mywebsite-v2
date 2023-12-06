7c797acc1abcd67495a0a9f3c60b45ffaaecdf23
florianbgt <florian@gutstickets.org>
2023-12-06 22:57:08 +0100

    fix deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: '@nuxt-themes/typography',
-  devtools: { enabled: true },
   modules: [
     'nuxt-content-assets', // make sure to add before content!
     '@nuxt/content',
